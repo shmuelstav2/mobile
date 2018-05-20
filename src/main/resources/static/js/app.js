20b071681fd1b7ce76d92daf83168a18c7240281
smulic <shmhad123@gmail.com>
2018-05-21 01:12:09 +0300

    security

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,17 +2,17 @@ var app = angular.module('app', []);
     app.controller('Ctrl', function($scope, $http) {
 
         function sikumtmuta(){
-            $http.get("http://localhost:8080/api/sikumtmuta/all")
+            $http.get("http://localhost:8010/api/sikumtmuta/all")
                 .then(function (response) {$scope.sikumtmuta = response.data;});
         }
 
         function sivukim(){
-            $http.get("http://localhost:8080/api/sivukim/all")
+            $http.get("http://localhost:8010/api/sivukim/all")
                 .then(function (response) {$scope.sivukim = response.data;});
         }
 
         function notrulesivuk(){
-            $http.get("http://localhost:8080/api/notrulesivuk/all")
+            $http.get("http://localhost:8010/api/notrulesivuk/all")
                 .then(function (response) {$scope.notrulesivuk = response.data;});
         }
 
